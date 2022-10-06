902f1bf9ccec8c468200f61611fb90e8c2cebbd5
Manav Raj <mr.manav.raj.2805@gmail.com>
2022-10-06 21:07:32 +0530

    Added redux

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+
+connectDB()
+
 const app=express();
 
 app.get('/', (req, res)=>{
     res.send('API is running..')
 })
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find(p=>p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running on port ${process.env.NODE_ENV} on ${PORT}`))
+app.listen(PORT, console.log(`Server running on port ${process.env.NODE_ENV} on ${PORT}`.yellow.bold))
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,12 +12,12 @@ const Header = () => {
                 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="ms-auto">
                         <LinkContainer to='/cart'>
-                            <Nav.Link ><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                            <Nav.Link ><i className='fas fa-shopping-cart'></i>  Cart</Nav.Link>
                         </LinkContainer>
                         <LinkContainer to='/login'>
-                            <Nav.Link ><i className='fas fa-user'></i>Sign In</Nav.Link>
+                            <Nav.Link ><i className='fas fa-user'></i>  Sign In</Nav.Link>
                         </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
